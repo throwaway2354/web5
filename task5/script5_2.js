81708ae9f1d23462d9aea2ed2899025348e80a7b
micha <micha030201@gmail.com>
2018-12-28 00:45:19 +1000

    put the non-jq version into a separate file

---
+++ task5/script5_2.js
@@ -0,0 +1,67 @@
+var m = [
+  {
+    text: 'Животные',
+    children: [
+      {
+        text: 'Млекопитающие',
+        children: [
+          { text: 'Коровы' },
+          { text: 'Ослы' },
+          { text: 'Собаки' },
+          { text: 'Тигры' },
+        ]
+      },
+      {
+        text: 'Другие',
+        children: [
+          { text: 'Змеи' },
+          { text: 'Птицы' },
+          { text: 'Ящерицы' },
+        ]
+      },
+    ]
+  },
+  {
+    text: 'Рыбы',
+    children: [
+      {
+        text: 'Аквариумные',
+        children: [
+          { text: 'Гуппи' },
+          { text: 'Скалярии' },
+        ]
+      },
+      {
+        text: 'Морские',
+        children: [
+          { text: 'Морская форель' },
+        ]
+      },
+    ]
+  },
+];
+
+function makeElements(template, ul) {
+  var li = ul.appendChild(document.createElement('li'));
+  li.appendChild(document.createTextNode(template.text));
+  if (template.children) {
+    var newUl = li.appendChild(document.createElement('ul'));
+    for (child of template.children) {
+      makeElements(child, newUl);
+    }
+  }
+}
+
+var ul = document.body.appendChild(document.createElement('ul'));
+for (child of m) {
+  makeElements(child, ul);
+}
+
+function listElements(el) {
+  console.log(el.tagName, el.firstChild.nodeValue);
+  for (child of el.children) {
+    listElements(child);
+  }
+}
+
+listElements(ul);
